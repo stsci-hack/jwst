e70222a6d587bacdaf256b37831e624281fc952c
Warren Hack <hack@stsci.edu>
2018-05-10 13:52:48 -0400

    Revising markers/fixtures for coron3

--- jwst/tests_nightly/general/pipelines/test_coron3_1.py
+++ jwst/tests_nightly/general/pipelines/test_coron3_1.py
@@ -2,21 +2,26 @@ import os
 import pytest
 from astropy.io import fits as pf
 from jwst.pipeline.calwebb_coron3 import Coron3Pipeline
+ 
+from jwst.helpers.io import get_bigdata
 
+"""
 pytestmark = [
     pytest.mark.usefixtures('_jail'),
     pytest.mark.skipif(not pytest.config.getoption('bigdata'),
                        reason='requires --bigdata')
 ]
-
-def test_coron3_pipeline1(_bigdata):
+"""
+@pytest.mark.usefixtures('_jail')
+#def test_coron3_pipeline1(_bigdata):
+def test_coron3_pipeline1():
     """Regression test of calwebb_coron3 pipeline.
 
     Test will be performed on NIRCam simulated data.
     """
-    subdir = os.path.join(_bigdata, 'nircam', 'test_coron3')
+    subdir = ['nircam', 'test_coron3']
     asn_name = 'jw99999-a3001_20170327t121212_coron3_001_asn.json'
-    asn_file = os.path.join(subdir, asn_name)
+    asn_file = get_bigdata(subdir, asn_name)
     override_psfmask = os.path.join(subdir, 'jwst_nircam_psfmask_somb.fits')
 
     pipe = Coron3Pipeline()
@@ -27,7 +32,7 @@ def test_coron3_pipeline1(_bigdata):
     # Compare psfstack product
     n_cur = 'jw99999-a3001_t1_nircam_f140m-maskbar_psfstack.fits'
     n_ref_name = 'jw99999-a3001_t1_nircam_f140m-maskbar_psfstack_ref.fits'
-    n_ref = os.path.join(subdir, n_ref_name)
+    n_ref = get_bigdata(subdir, n_ref_name)
 
     h = pf.open(n_cur)
     href = pf.open(n_ref)
@@ -45,7 +50,8 @@ def test_coron3_pipeline1(_bigdata):
     # Compare psfalign product
     n_cur = 'jw9999947001_02102_00001_nrcb3_a3001_psfalign.fits'
     n_ref_name = 'jw99999-a3001_t1_nircam_f140m-maskbar_psfalign_ref.fits'
-    n_ref = os.path.join(subdir, n_ref_name)
+    n_ref = get_bigdata(subdir, n_ref_name)
+
     h = pf.open(n_cur)
     href = pf.open(n_ref)
     newh = pf.HDUList([h['primary'], h['sci'], h['err'], h['dq']])
@@ -61,7 +67,7 @@ def test_coron3_pipeline1(_bigdata):
     # Compare psfsub product
     n_cur = 'jw9999947001_02102_00001_nrcb3_a3001_psfsub.fits'
     n_ref_name = 'jw9999947001_02102_00001_nrcb3_psfsub_ref.fits'
-    n_ref = os.path.join(subdir, n_ref_name)
+    n_ref = get_bigdata(subdir, n_ref_name)
 
     h = pf.open(n_cur)
     href = pf.open(n_ref)
@@ -78,7 +84,7 @@ def test_coron3_pipeline1(_bigdata):
     # Compare level-2c product
     n_cur = 'jw9999947001_02102_00001_nrcb3_a3001_crfints.fits'
     n_ref_name = 'jw9999947001_02102_00001_nrcb3_a3001_crfints_ref.fits'
-    n_ref = os.path.join(subdir, n_ref_name)
+    n_ref = get_bigdata(subdir, n_ref_name)
 
     h = pf.open(n_cur)
     href = pf.open(n_ref)
@@ -94,7 +100,7 @@ def test_coron3_pipeline1(_bigdata):
 
     n_cur = 'jw9999947001_02102_00002_nrcb3_a3001_crfints.fits'
     h = pf.open(n_cur)
-    n_ref = os.path.join(subdir,
+    n_ref = get_bigdata(subdir,
                         'jw9999947001_02102_00002_nrcb3_a3001_crfints_ref.fits')
 
     href = pf.open(n_ref)
@@ -109,7 +115,7 @@ def test_coron3_pipeline1(_bigdata):
 
     # Compare i2d product
     n_cur = 'jw99999-a3001_t1_nircam_f140m-maskbar_i2d.fits'
-    n_ref = os.path.join(subdir,
+    n_ref = get_bigdata(subdir,
                          'jw99999-a3001_t1_nircam_f140m-maskbar_i2d_ref.fits')
 
     h = pf.open(n_cur)
